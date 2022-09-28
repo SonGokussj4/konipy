34f083a50f36e1f7d4cd1a3a518c92b6f0bcb437
SonGokussj4 <SonGokussj4@centrum.cz>
2022-09-28 20:52:16 +0200

    Apply drawings into all images

--- comp_tracking/main.py
+++ comp_tracking/main.py
@@ -40,6 +40,7 @@ def main():
     print("Hello world!")
 
     shop_path = CURDIR / "shop"
+    output_dir = (CURDIR / "shop").parent
     print("shop_path:", shop_path)
 
     imgs = load_images_from_dir((shop_path / "*.jpg").as_posix())
@@ -47,62 +48,63 @@ def main():
     xmls = load_xml_from_dir((shop_path / "*.xml").as_posix())
     print(f"[ DEBUG ] xmls: {xmls}")
 
-    # Assign random IDs to the bounding boxes in first xml
-    first_picture = DataFrame(xmls[0], shop_path)
-    first_picture.assign_ids()
-    print(f"[ DEBUG ] first: {first_picture}")
+    for xml in xmls:
+        # Assign random IDs to the bounding boxes in first xml
+        picture = DataFrame(xml, shop_path)
+        picture.assign_ids()
+        print(f"[ DEBUG ] first: {picture}")
 
-    # Send the first picture to the server
-    send_to_server(first_picture.to_json())
+        # Send the first picture to the server
+        send_to_server(picture.to_json())
 
-    # TEST_IMAGE_PATH = shop_path / "TEST_IMAGE.jpg"
-    TEST_IMAGE_PATH = shop_path / first_picture.img_name
-    print(f"[ DEBUG ] TEST_IMAGE_PATH: {TEST_IMAGE_PATH}")
+        TEST_IMAGE_PATH = shop_path / picture.img_name
+        print(f"[ DEBUG ] TEST_IMAGE_PATH: {TEST_IMAGE_PATH}")
 
-    image = cv2.imread(TEST_IMAGE_PATH.as_posix())
+        image = cv2.imread(TEST_IMAGE_PATH.as_posix())
 
-    for item in first_picture.boxes:
-        box = item["box"]
-        _id = item["id"]
-        color = COLOR_PALLETE[_id]
-        # Create a rectangle around the object
-        box_start_point = (int(box[0]), int(box[1]))  # left top corner
-        box_end_point = (int(box[2]), int(box[3]))  # right bottom corner
-        cv2.rectangle(img=image, pt1=box_start_point, pt2=box_end_point, color=color, thickness=2)
+        for item in picture.boxes:
+            box = item["box"]
+            _id = item["id"]
+            color = COLOR_PALLETE[_id]
+            # Create a rectangle around the object
+            box_start_point = (int(box[0]), int(box[1]))  # left top corner
+            box_end_point = (int(box[2]), int(box[3]))  # right bottom corner
+            cv2.rectangle(img=image, pt1=box_start_point, pt2=box_end_point, color=color, thickness=2)
 
-        # Display ID of the box on the top of the rectangle
-        label_text = f"ID: {_id}"
+            # Display ID of the box on the top of the rectangle
+            label_text = f"ID: {_id}"
 
-        # Get text size to know how long the "background rectangle" should be
-        (text_box_x, text_box_y), _ = cv2.getTextSize(
-            label_text, cv2.FONT_HERSHEY_SIMPLEX, LABEL_FONT_SCALE, LABEL_FONT_THICKNESS
-        )
+            # Get text size to know how long the "background rectangle" should be
+            (text_box_x, text_box_y), _ = cv2.getTextSize(
+                label_text, cv2.FONT_HERSHEY_SIMPLEX, LABEL_FONT_SCALE, LABEL_FONT_THICKNESS
+            )
 
-        label_x = box_start_point[0] + 5
-        label_y = box_start_point[1] + 5
+            label_x = box_start_point[0] + 5
+            label_y = box_start_point[1] + 5
 
-        # Create a rectangle around the text
-        cv2.rectangle(
-            img=image,
-            pt1=(label_x - 5, label_y + 5),
-            pt2=(label_x + text_box_x + 5, label_y - text_box_y - 5),
-            color=color,
-            thickness=cv2.FILLED,
-        )
+            # Create a rectangle around the text
+            cv2.rectangle(
+                img=image,
+                pt1=(label_x - 5, label_y + 5),
+                pt2=(label_x + text_box_x + 5, label_y - text_box_y - 5),
+                color=color,
+                thickness=cv2.FILLED,
+            )
 
-        # Display the text
-        cv2.putText(
-            img=image,
-            text=label_text,
-            org=(label_x, label_y),
-            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-            fontScale=LABEL_FONT_SCALE,
-            color=(0, 0, 0),
-            thickness=LABEL_FONT_THICKNESS,
-        )
+            # Display the text
+            cv2.putText(
+                img=image,
+                text=label_text,
+                org=(label_x, label_y),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=LABEL_FONT_SCALE,
+                color=(0, 0, 0),
+                thickness=LABEL_FONT_THICKNESS,
+            )
 
-    # Save image
-    cv2.imwrite(f"{first_picture.img_name}_OUTPUT.jpg", image)
+        # Save image
+        cv2.imwrite(f"{output_dir / picture.img_name}_OUTPUT.jpg", image)
+
 
     raise SystemExit
 
